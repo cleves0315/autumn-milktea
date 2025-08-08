072975e2b03dc52ef513ba15f9affaf6102a2e6b
cleves0315 <cleves0315@foxmail.com>
2025-08-08 14:51:19 +0800

    feat: add delays to animations for improved timing and flow

--- src/animate.ts
+++ src/animate.ts
@@ -1,23 +1,27 @@
 import { animate as ant } from 'motion'
 
 export const animate = async () => {
-  ant('#cup-btm', { y: 0 })
+  ant('#cup-btm', { y: 0 }, { delay: 0.2 })
   ant('#cup-body', { y: 0 }, { delay: 0.6 })
-  await ant('#cup-lid', { y: 0 }, { delay: 0.8 })
+  await ant('#cup-lid', { y: 0 }, { delay: 1 })
 
-  ant('#love', { x: 0 }, { type: 'tween' })
-  await ant('#sig', { x: 0 }, { type: 'tween', delay: 0.2 })
+  ant('#love', { x: 0 }, { type: 'tween', delay: 0.2 })
+  await ant('#sig', { x: 0 }, { type: 'tween', delay: 0.6 })
 
-  ant('#autumns', { x: 0, y: 0, rotate: 360 * 4 }, { type: 'tween' })
+  ant(
+    '#autumns',
+    { x: 0, y: 0, rotate: 360 * 4 },
+    { type: 'tween', delay: 0.2 }
+  )
   await ant(
     '#milktea',
     { x: 0, y: 0, rotate: 360 * 4 },
-    { type: 'tween', delay: 0.2 }
+    { type: 'tween', delay: 0.6 }
   )
 
   ant(
     '#forSavage',
     { clipPath: 'inset(0 0 0 0)' },
-    { duration: 2, type: 'tween' }
+    { duration: 2, type: 'tween', delay: 0.4 }
   )
 }
